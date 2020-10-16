60df0b7a14fc0da2b1220654f5070ce06f77ef12
szymonszoldra <szymon.szoldra@gmail.com>
2020-10-16 23:35:26 +0200

    add create part

--- database.py
+++ database.py
@@ -7,8 +7,18 @@ class Database:
         self.cursor = self.connection.cursor()
 
     def initialize(self, query):
-        self.cursor.execute(query)
-        self.connection.commit()
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except sqlite3.Error as e:
+            print(e)
+
+    def create(self, query, name, surname, mark):
+        try:
+            self.cursor.execute(query, (None, name, surname, mark))
+            self.connection.commit()
+        except sqlite3.Error as e:
+            print(e)
 
     def __del__(self):
         self.connection.close()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from database import Database
 from os import getenv
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 load_dotenv()
 
@@ -22,3 +22,19 @@ def setup_database():
     db = Database(name)
     db.initialize('CREATE TABLE students (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, mark INTEGER)')
     return 'Initialized table students in database'
+
+
+@app.route('/create', methods=['POST'])
+def add_student():
+    req = request.get_json()
+    db_name = getenv('DB_NAME')
+    db = Database(db_name)
+    name = req['name']
+    surname = req['surname']
+    mark = req['mark']
+
+    query = f'INSERT INTO students (id ,name, surname, mark) VALUES (?,?,?,?)'
+    db.create(query, name, surname, mark)
+    return 'Insertion complete'
+
+
